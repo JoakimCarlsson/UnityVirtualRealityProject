92394c24b15c7f7efa0520d2e58d7754a1558788
Joakim Carlsson <Carlsson@DESKTOP-V8MGHQQ>
2020-10-09 15:51:04 +0200

    Now we can teleport with the joystick on the left controller.

---
+++ Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController leftTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+    private XRRayInteractor _leftRayInteractor;
+
+    private void Start()
+    {
+        _leftRayInteractor = leftTeleportRay.gameObject.GetComponent<XRRayInteractor>();
+    }
+    void Update()
+    {
+        if (leftTeleportRay)
+        {
+            _leftRayInteractor.allowSelect = CheckIfActivated(leftTeleportRay);
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+    }
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
