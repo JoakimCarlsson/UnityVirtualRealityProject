6069f812796d7bf6f7d47c6e23184c86f9523c83
Joakim Carlsson <Carlsson@DESKTOP-V8MGHQQ>
2020-10-11 15:22:31 +0200

    Added a very basic weapon that can shoot projectiles.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    public float destroyAfter = 5f;
+    public bool destoryOnImpact = false;
+    public float minDestoryTime = 0.01f;
+    public float maxDestoryTime = 0.05f;
+
+    [Header("Impact Effects")] 
+    public Transform[] impactPrefabs;
+
+    private Rigidbody _rigidbody;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.drag = 0.65f;
+        _rigidbody.angularDrag = 0.05f;
+
+        StartCoroutine(DestroyAfter());
+    }
+
+    private IEnumerator DestroyAfter()
+    {
+        yield return new WaitForSeconds(destroyAfter);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (destoryOnImpact)
+            Destroy(gameObject);
+        else
+            StartCoroutine(DestroyTimer());
+
+        //different impacts for different 'materials' using impactPrefabs later.
+    }
+
+    private IEnumerator DestroyTimer()
+    {
+        yield return new WaitForSeconds(Random.Range(minDestoryTime, maxDestoryTime));
+    }
+}
